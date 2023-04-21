cb145f8aff3fe0a8b7d62578944e315eb716a0b1
AnhNT <tien226anh@gmail.com>
2023-04-21 09:21:06 +0700

    commit

--- app/books/routers.py
+++ app/books/routers.py
@@ -4,8 +4,7 @@ from bson import ObjectId
 from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, status, Body
 from fastapi.responses import JSONResponse
 from app.books.models import AddBookModel, UpdateModel, BookModel
-from app.books.services import count_books, create_book, find_books_by_filter_and_paginate, update_book, \
-    find_books_not_paginate
+from app.books.services import count_books, create_book, find_books_by_filter_and_paginate, update_book, find_books_not_paginate
 
 # from fastapi_jwt_auth import AuthJWT
 
@@ -25,14 +24,25 @@ async def add_new_book(body: AddBookModel):
 
 
 @router.get("/")
-async def get_books(title: str = '', skip=1, limit=20):
+async def get_books(
+    title: str = '',
+    author: str = '',
+    category: str = '',
+    skip: int = 1,
+    limit: int = 20,
+):
     query = {}
     if title:
-        query = {"title": {"$regex": f'{title}', "$options": "i"}}
-    books = await find_books_by_filter_and_paginate(query, int(skip), int(limit))
+        query["title"] = {"$regex": title, "$options": "i"}
+    if author:
+        query["author"] = {"$regex": author, "$options": "i"}
+    if category:
+        query["category"] = {"$regex": category, "$options": "i"}
+    books = await find_books_by_filter_and_paginate(query, skip, limit)
     count = await count_books(query)
     return JSONResponse(
-        status_code=status.HTTP_200_OK, content={"result": books, "total_record": count, }
+        status_code=status.HTTP_200_OK,
+        content={"result": books, "total_record": count},
     )
 # https://dev.to/franciscomendes10866/how-to-create-a-table-with-pagination-in-react-4lpd
 # Paginate
